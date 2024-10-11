f0577be0e6808b943971f1f719f74a9796069a6c
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2024-10-11 18:13:40 +0800

    Add files via upload

---
+++ Data_preprocessing/R/tcga_gene_expression_download.R
@@ -0,0 +1,212 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("TCGAbiolinks")
+library(TCGAbiolinks)
+query <- GDCquery(project = "TCGA-LAML", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-ACC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-BLCA", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-LGG", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-BRCA", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-CESC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-CHOL", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+
+query <- GDCquery(project = "TCGA-COAD", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-ESCA", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-GBM", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-HNSC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-KICH", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-KIRC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-KIRP", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-LIHC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-LUAD", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-LUSC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-DLBC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+
+query <- GDCquery(project = "TCGA-MESO", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-OV", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-PAAD", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-PCPG", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-PRAD", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-READ", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-SARC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-SKCM", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-STAD", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+library(TCGAbiolinks)
+query <- GDCquery(project = "TCGA-TGCT", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-THYM", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-THCA", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-UCS", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-UCEC", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - FPKM")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+query <- GDCquery(project = "TCGA-UVM", 
+                    data.category = "Transcriptome Profiling", 
+                    data.type = "Gene Expression Quantification", 
+                    workflow.type = "HTSeq - Counts")
+GDCdownload(query, method = "api", directory = "Data_preprocessing/Origin_Data/RNASeq", files.per.chunk = 100)
+
+
+
+
+
+
+
+
